35eb73c36dffaaeec95c50ddcbb33ce9d7564435
Rahul Mirashi <rpmirashi@gmail.com>
2020-12-06 00:37:13 +0530

    validations and registration done

---
+++ css/home.php
@@ -0,0 +1,20 @@
+<?php 
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <?php include('template/header.php');?>
+<body>
+    <div class="center">
+        <img src="img/bank_logo.jpg" alt="Bank">
+    </div>
+    <div class="container-fluid bg-dark">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+</body>
+    <?php include('template/footer.php');?>
+</html>
