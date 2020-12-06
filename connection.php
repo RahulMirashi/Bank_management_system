b2aebe1a1f5653674a2fdb958360ca56bc367c2b
Rahul Mirashi <rpmirashi@gmail.com>
2020-12-06 16:02:58 +0530

    connection updated

--- connection.php
+++ connection.php
@@ -1,18 +1,9 @@
 <?php 
 
-<<<<<<< Updated upstream
 	$conn = new mysqli("localhost","root","","bank");
 
-<<<<<<< Updated upstream
-?>
-=======
-	$conn = new mysqli("localhost","root","","test");
-
- ?>
->>>>>>> Stashed changes
-=======
 	if(!$conn){
 		die('Please check yout connection'.mysqli_error());
 	}
+?>
 
->>>>>>> Stashed changes
