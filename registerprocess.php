72d53f3e9b8692eb509f8f1dabf2d27862c714e2
BhargavMishal <bhargavmishal1111@gmail.com>
2020-12-07 11:24:25 +0530

    Final without css

--- registerprocess.php
+++ registerprocess.php
@@ -5,7 +5,7 @@
     $clname = $_POST['clname'];
     $caddress = $_POST['caddress'];
     $cpass = $_POST['cpass'];
-    
+
     $sql = "INSERT INTO `customer`(`cid`, `cfname`, `clname`, `caddress`, `cpass`) VALUES ('".$cid."','".$cfname."','".$clname."','".$caddress."','".$cpass."')";
     $res = mysqli_query($conn,$sql);
 
@@ -29,11 +29,11 @@
     $res = mysqli_query($conn,$sql);
     if(!$res)
     {
-        //header("Location:home.php?register_status=success"); 
+        header("Location:home.php?register_status=success"); 
     }
     else
     {
-        //header("Location:register.php?register_status=Unable to create user check ur cid");
+        header("Location:register.php?register_status=Unable to create user check ur cid");
     }
     
 
