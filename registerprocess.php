512cd76ac07053e938b0d5ce01c81743c1892243
BhargavMishal <bhargavmishal1111@gmail.com>
2020-12-07 11:15:23 +0530

    Register creates acc

--- registerprocess.php
+++ registerprocess.php
@@ -1,25 +1,39 @@
 <?php 
     include('connection.php');
-            
     $cid = $_POST['cid'];
     $cfname = $_POST['cfname'];
     $clname = $_POST['clname'];
     $caddress = $_POST['caddress'];
     $cpass = $_POST['cpass'];
-
-    //  $a = (int)$cid;
+    
     $sql = "INSERT INTO `customer`(`cid`, `cfname`, `clname`, `caddress`, `cpass`) VALUES ('".$cid."','".$cfname."','".$clname."','".$caddress."','".$cpass."')";
     $res = mysqli_query($conn,$sql);
-    // while($row = mysqli_fetch_assoc($res))
-    // {
-    print_r($res);
-    if(!mysqli_error($res))
+
+    $sql = "SELECT max(accno) AS maxacc FROM bank_account";
+    $accno_sql = mysqli_query($conn,$sql);
+    while($row = mysqli_fetch_assoc($accno_sql))
     {
-        header("Location:home.php?register_status=success"); 
+        $accno = $row['maxacc'];
+    };
+    $accno = $accno + 1;
+    echo $accno;
+    $sql = "SELECT max(cardno) AS maxcard FROM bank_account";
+    $cardno_sql = mysqli_query($conn,$sql);
+    while($row = mysqli_fetch_assoc($cardno_sql))
+    {
+        $cardno = $row['maxcard'];
+    };
+    $cardno = $cardno + 1;
+
+    $sql = "INSERT INTO `bank_account` (`accno`, `balance`, `cid`, `cardno`) VALUES ('".$accno."','0','".$cid."','".$cardno."');";
+    $res = mysqli_query($conn,$sql);
+    if(!$res)
+    {
+        //header("Location:home.php?register_status=success"); 
     }
     else
     {
-        header("Location:register.php?register_status=Unable to create user check ur cid");
+        //header("Location:register.php?register_status=Unable to create user check ur cid");
     }
     
 
